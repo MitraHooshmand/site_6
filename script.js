b3faeb7d3d43340b449e9d79f46eb375a3a148a9
mita4824 <87270333+mita4824@users.noreply.github.com>
2022-05-22 20:47:46 +0430

    improve css and make the buttons work

--- script.js
+++ script.js
@@ -5,5 +5,16 @@ const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnShowModal = document.querySelectorAll(".show-modal");
 
+const showModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
 for (let i = 0; i < btnShowModal.length; i++)
-  console.log(btnShowModal[i].textContent);
+  btnShowModal[i].addEventListener("click", function () {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  });
+
+btnCloseModal.addEventListener("click", showModal);
+overlay.addEventListener("click", showModal);
