df95d6e90f006844ce0cfd7f13cbed317b5b9796
mita4824 <87270333+mita4824@users.noreply.github.com>
2022-05-22 20:06:48 +0430

    create index and style pages

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnShowModal = document.querySelectorAll(".show-modal");
-console.log('s');
+
 for (let i = 0; i < btnShowModal.length; i++)
   console.log(btnShowModal[i].textContent);
