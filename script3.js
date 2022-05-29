26ec4e0173ccc219ae060b07e25693e1dcd6d9eb
mita4824 <87270333+mita4824@users.noreply.github.com>
2022-05-29 23:28:59 +0430

    code struct

--- script3.js
+++ script3.js
@@ -4,3 +4,25 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector('.overlay');
 const btnClose = document.querySelector(".close-modal");
 const btnShow = document.querySelectorAll(".show-modal");
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+for (let i = 0 ; i < btnShow.length ; i++){
+
+    btnShow[i].addEventListener("click", openModal);
+}
+btnClose.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+document.addEventListener('keydown',function(param){
+  if(param.key === 'Escape' && !modal.classList.contains('hidden')){
+    
+    closeModal();
+  }
+})
