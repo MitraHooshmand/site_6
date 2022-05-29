97357b577f32d59dd9c0dc7c36726aaa8412c5f3
mita4824 <87270333+mita4824@users.noreply.github.com>
2022-05-29 22:59:21 +0430

    practice again

--- script3.js
+++ script3.js
@@ -1,25 +1,6 @@
-"use strict";
+'use stricts';
 
 const modal = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay");
-const btnCloseModal = document.querySelector(".close-modal");
-const btnShowModal = document.querySelectorAll(".show-modal");
-const closeModal = function () {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
-};
-const openModal = function () {
-  modal.classList.remove("hidden");
-  overlay.classList.remove("hidden");
-};
-
-for (let i = 0; i < btnShowModal.length; i++) {
-  btnShowModal[i].addEventListener("click", openModal);
-}
-btnCloseModal.addEventListener("click", closeModal);
-overlay.addEventListener("click", closeModal);
-document.addEventListener("keydown", function (event) {
-  if (event.key === "Escape" && !modal.classList.contains("hidden")) {
-    closeModal();
-  }
-});
+const overlay = document.querySelector('.overlay');
+const btnClose = document.querySelector(".close-modal");
+const btnShow = document.querySelectorAll(".show-modal");
